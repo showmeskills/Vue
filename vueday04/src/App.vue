05dc1740c815d4484e037af47d7867f2a0a6e440
Terrance <465446853yan@gmail.com>
2020-12-09 19:20:34 +1100

    this is a demo about dropdown to using the method of props

---
+++ vueday04/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header>
+      <Navbar />
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+
+export default{
+  name: "app",
+  components:{
+    Navbar
+  }
+}
+
+</script>
+
+<style lang="less">
+*{
+  margin: 0;
+  padding:0;
+  box-sizing:border-box;
+}
+body{
+  font-family:'monterrat',sans-serif;
+}
+header{
+  width:100vw;
+  background: #222;
+  padding: 15px;
+}
+</style>
